46d49f388a06306ecb372d072ea2c4a7efc6f09f
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-07 19:28:04 -0300

    feat: Create table insert simulation

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 require('dotenv').config()
 
@@ -8,9 +9,13 @@ const PORT_NUMBER: number =
   parseInt(<string>process.env.PORT_NUMBER, 10) || 3333
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions').insert({
+    id: crypto.randomUUID(),
+    title: 'Transaction test 1',
+    amount: 1000,
+  })
 
-  return tables
+  return transaction
 })
 
 app
