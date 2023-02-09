afbc7a204046f0621ddff1a3caa275ea42270c08
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-09 16:22:07 -0300

    feat: Migration - create users table

--- db/migrations/20230208231023_create-users-table.ts
+++ db/migrations/20230208231023_create-users-table.ts
@@ -1,5 +1,17 @@
 import { Knex } from 'knex'
 
-export async function up(knex: Knex): Promise<void> {}
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('deleted_at').defaultTo(knex.fn.now()).notNullable()
+    table.text('email').unique().notNullable()
+    table.text('password').notNullable()
+    table.boolean('active').defaultTo(true).notNullable()
+  })
+}
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
+}
