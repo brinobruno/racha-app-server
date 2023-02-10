4e602956ebd4308e66a119f6f52d5f33ca3dd84d
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-09 21:18:35 -0300

    feat: Add @fastify/cookie to server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import { env } from './env'
 import fastify from 'fastify'
 import { usersRoutes } from './routes/users'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
+
+/* Cookie is registered before so that it is accessible to everything past here */
+app.register(cookie)
 
 app.register(usersRoutes, {
   prefix: 'users',
