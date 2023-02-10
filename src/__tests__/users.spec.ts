4c967f47a99f7ec104355e4f083f44c554d0d3ae
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-10 15:33:22 -0300

    feat: Create users.spec for testing users routes

---
+++ src/__tests__/users.spec.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it, beforeAll, afterAll } from '@jest/globals'
+import request from 'supertest'
+import { app } from '../app'
+
+describe('Users routes', () => {
+  /* Make sure app (and thefore its routes) are done loading before testing */
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('Should be able to create a new account', async () => {
+    const response = await request(app.server).post('/users/create').send({
+      email: 'account12@jest.com',
+      password: 'weakpasssword123',
+    })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
