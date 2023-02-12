5c9c761ba0932321e99182572d54dc36a07ae498
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-12 16:53:56 -0300

    fix: Add setUserParamsSchema to schemas

--- src/modules/users/users.schemas.ts
+++ src/modules/users/users.schemas.ts
@@ -1,5 +1,11 @@
 import { z } from 'zod'
 
+export function setUserParamsSchema() {
+  return z.object({
+    id: z.string().uuid(),
+  })
+}
+
 export const createUserBodySchema = z.object({
   email: z.string().email(),
   password: z.string(),
