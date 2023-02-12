a5d21dc39b8f5013aaf2bb6b3b36ed5e77d097f9
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-12 19:38:01 -0300

    feat: Create updateUserBodySchema

--- src/modules/users/users.schemas.ts
+++ src/modules/users/users.schemas.ts
@@ -11,6 +11,11 @@ export const createUserBodySchema = z.object({
   password: z.string(),
 })
 
+export const updateUserBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
 export const loginUserBodySchema = z.object({
   email: z.string().email(),
   password: z.string(),
