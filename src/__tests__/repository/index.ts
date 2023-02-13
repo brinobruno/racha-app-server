d414aff198c5485215a26d2f277883912e24fe34
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-13 17:39:28 -0300

    tests: Create static, fake data for testing to avoid magic numbers

---
+++ src/__tests__/repository/index.ts
@@ -0,0 +1,9 @@
+const USER_EMAIL = 'test@jest.com'
+
+const USER_PASSWORD = 'weakpassword123'
+
+const USER_UPDATED_EMAIL = 'updated@jest.com'
+
+const USER_UPDATED_PASSWORD = 'strongpassword456'
+
+export { USER_EMAIL, USER_PASSWORD, USER_UPDATED_EMAIL, USER_UPDATED_PASSWORD }
