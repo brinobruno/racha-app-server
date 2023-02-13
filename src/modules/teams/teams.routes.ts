888ef9d58defaa78998c500192f4ec7d12c84ad0
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-13 20:28:51 -0300

    feat: Create teamsRoutes with /create

---
+++ src/modules/teams/teams.routes.ts
@@ -0,0 +1,14 @@
+import { FastifyInstance } from 'fastify'
+
+import { createTeamHandler } from './teams.controllers'
+import { checkSessionIdExists } from '../../middlewares/check-session-id-exists'
+
+export async function teamsRoutes(app: FastifyInstance) {
+  app.post(
+    '/create',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    createTeamHandler,
+  )
+}
