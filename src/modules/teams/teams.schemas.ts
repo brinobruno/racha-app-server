1e2d98fad7ebafcded5f1a42f42717ac56dfdd8c
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-14 18:14:22 -0300

    feat: Add setTeamParamsSchema

--- src/modules/teams/teams.schemas.ts
+++ src/modules/teams/teams.schemas.ts
@@ -7,7 +7,20 @@ export const createTeamBodySchema = z.object({
   active: z.boolean().default(true),
 })
 
+export const updateTeamBodySchema = z.object({
+  title: z.string().min(2).max(255),
+  owner: z.string().min(2).max(255),
+  badge_url: z.string().optional(),
+  active: z.boolean().default(true),
+})
+
 export function setUserParamsSchema() {
+  return z.object({
+    id: z.string().uuid(),
+  })
+}
+
+export function setTeamParamsSchema() {
   return z.object({
     id: z.string().uuid(),
   })
