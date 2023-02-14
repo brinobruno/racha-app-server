bb0b5a5057458f58f99d8d852dd9c82b2b8e4cb2
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-14 17:38:12 -0300

    feat: Add teams mockup data

--- src/mockup-repository/index.ts
+++ src/mockup-repository/index.ts
@@ -1,3 +1,4 @@
+/* Users */
 const USER_EMAIL = 'test@jest.com'
 
 const USER_PASSWORD = 'weakpassword123'
@@ -6,4 +7,19 @@ const USER_UPDATED_EMAIL = 'updated@jest.com'
 
 const USER_UPDATED_PASSWORD = 'strongpassword456'
 
-export { USER_EMAIL, USER_PASSWORD, USER_UPDATED_EMAIL, USER_UPDATED_PASSWORD }
+/* Teams */
+const TEAM_TITLE = 'Blackburn Bosque'
+
+const TEAM_OWNER = '777 Partners'
+
+const TEAM_BADGE_URL = 'https://github.com/brinobruno.png'
+
+export {
+  USER_EMAIL,
+  USER_PASSWORD,
+  USER_UPDATED_EMAIL,
+  USER_UPDATED_PASSWORD,
+  TEAM_TITLE,
+  TEAM_OWNER,
+  TEAM_BADGE_URL,
+}
