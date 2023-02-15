413f0cda55e01867ac877c434af55e882d0d4b66
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-15 19:26:51 -0300

    feat: Add getSessionById helper and update check-session-id-exists

--- src/middlewares/check-session-id-exists.ts
+++ src/middlewares/check-session-id-exists.ts
@@ -1,5 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 
+import { getSessionById } from './helper/getSessionById'
+
 export async function checkSessionIdExists(
   request: FastifyRequest,
   reply: FastifyReply,
@@ -8,7 +10,16 @@ export async function checkSessionIdExists(
 
   if (!sessionId) {
     return reply.status(401).send({
-      error: 'Unauthorized: No cookies present',
+      error: 'Unauthorized: No session ID present',
+    })
+  }
+
+  // Retrieve the session information from the database
+  const session = await getSessionById(sessionId)
+
+  if (!session) {
+    return reply.status(401).send({
+      error: 'Unauthorized: Invalid session ID',
     })
   }
 }
---
+++ src/middlewares/helpers/getSessionById.ts
@@ -0,0 +1,21 @@
+import { knex } from '../../database'
+
+interface IUserSession {
+  id: string
+  sessionId: string
+}
+
+export async function getSessionById(
+  sessionId: string,
+): Promise<IUserSession | null> {
+  const userSession = await knex('users').where('session_id', sessionId).first()
+
+  if (!userSession?.session_id) {
+    return null
+  }
+
+  return {
+    id: userSession.id,
+    sessionId: userSession.session_id,
+  }
+}
