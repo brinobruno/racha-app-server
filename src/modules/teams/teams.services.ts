887f3325538edce09392bce8d34c824b941b7c48
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-16 17:27:45 -0300

    feat: Verify if id from user is the same as the teams user_id to proceed

--- src/modules/teams/teams.services.ts
+++ src/modules/teams/teams.services.ts
@@ -7,7 +7,7 @@ import { createTeamBodySchema, updateTeamBodySchema } from './teams.schemas'
 import { HttpError } from '../../errors/customException'
 
 export async function createTeam(
-  id: string,
+  userId: string,
   input: z.infer<typeof createTeamBodySchema>,
 ) {
   const { title, owner, badge_url } = input
@@ -28,7 +28,7 @@ export async function createTeam(
       title,
       owner,
       badge_url,
-      user_id: id,
+      user_id: userId,
     })
     .returning('id')
 
@@ -61,10 +61,21 @@ export async function deleteTeamById(id: string) {
 export async function updateTeam(
   input: z.infer<typeof updateTeamBodySchema>,
   id: string,
+  userId: string | undefined,
 ) {
   const { title, owner, badge_url, active } = input
 
-  const teamToUpdate = await knex('teams')
+  const teamToUpdate = await knex('teams').where({ id }).first()
+
+  // Check if the team's user_id matches the user's id
+  if (teamToUpdate.user_id !== userId) {
+    throw new HttpError(
+      401,
+      'Permission denied, ID from user does not match team user_id ',
+    )
+  }
+
+  const updatedTeam = await knex('teams')
     .where({ id })
     .update({
       title,
@@ -74,5 +85,5 @@ export async function updateTeam(
     })
     .returning('id')
 
-  return teamToUpdate
+  return updatedTeam
 }
