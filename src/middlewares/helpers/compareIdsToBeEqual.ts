2f8421a1ec035918d8572714283e185434a501b8
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-17 15:24:07 -0300

    feat: Create helper function to compareIdsToBeEqual to replace duplications

---
+++ src/middlewares/helpers/compareIdsToBeEqual.ts
@@ -0,0 +1,16 @@
+import { HttpError } from '../../errors/customException'
+
+interface ICompareIds {
+  secondId: string | undefined
+  firstId: string | undefined
+}
+
+// Compare two IDs to be the same
+export function compareIdsToBeEqual({ firstId, secondId }: ICompareIds) {
+  if (firstId !== secondId) {
+    throw new HttpError(
+      401,
+      'Permission denied, ID from user does not match team user_id ',
+    )
+  }
+}
