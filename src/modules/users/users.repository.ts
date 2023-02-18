54dca432013b6405ad19bbccb44cf7c2e80d9752
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-18 20:17:27 -0300

    feat: Add findUserById method

--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -2,10 +2,15 @@ import { knex } from '../../database'
 
 export interface IUserRepository {
   findUsers(): Promise<object[]>
+  findUserById(id: string): Promise<unknown>
 }
 
 export const userRepository: IUserRepository = {
   async findUsers() {
     return await knex('users').select('*')
+  },
+
+  async findUserById(id: string) {
+    return await knex('users').where({ id }).first()
   },
 }
