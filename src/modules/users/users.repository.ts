a32ba65b6d2bfc13ac7fd9105ecb09fe5c8ae701
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-18 20:31:49 -0300

    feat: Add checkUserAlreadyExists method to repository

--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -3,6 +3,7 @@ import { knex } from '../../database'
 export interface IUserRepository {
   findUsers(): Promise<object[]>
   findUserById(id: string): Promise<unknown>
+  checkUserAlreadyExists(email: string): Promise<object | undefined>
 }
 
 export const userRepository: IUserRepository = {
@@ -12,5 +13,13 @@ export const userRepository: IUserRepository = {
 
   async findUserById(id: string) {
     return await knex('users').where({ id }).first()
+  },
+
+  async checkUserAlreadyExists(email: string) {
+    return await knex
+      .select('email')
+      .from('users')
+      .where('email', email)
+      .first()
   },
 }
