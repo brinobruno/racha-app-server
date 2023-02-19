fff5c8420a1abdc6bdfd9347359bab7c6ca1b224
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-19 20:50:31 -0300

    chore: Remove /helpers from /middlewares

--- src/middlewares/helpers/verifySessionId.ts
+++
@@ -1,7 +0,0 @@
-import { HttpError } from '../../errors/customException'
-
-export function verifySessionId(sessionId: string | undefined) {
-  if (!sessionId) {
-    throw new HttpError(401, 'Unauthorized: No session ID present')
-  }
-}
