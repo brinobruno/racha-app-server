994a4a4d14785922e85b8943b85017188157ed72
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-20 19:49:20 -0300

    feat: Add findPlayers

--- src/modules/players/players.services.ts
+++ src/modules/players/players.services.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 
 import { createPlayerBodySchema } from './players.schemas'
 import { playerRepository } from './players.repository'
+import { HttpError } from '../../errors/customException'
+import { teamRepository } from '../teams/teams.repository'
 
 export async function createPlayer(
   input: z.infer<typeof createPlayerBodySchema>,
@@ -11,4 +13,12 @@ export async function createPlayer(
   const playerToCreate = await playerRepository.createPlayerById(input, teamId)
 
   return playerToCreate
+}
+
+export async function findPlayers(teamId: string) {
+  const team = await teamRepository.getTeamById(teamId)
+
+  if (!team) throw new HttpError(404, 'Team not found')
+
+  return await playerRepository.findPlayersFromTeamId(teamId)
 }
