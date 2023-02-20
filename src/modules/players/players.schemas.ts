1ea487d67da06abbfe86d1ef9afbc8618ef5a9cc
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-20 19:21:31 -0300

    feat: Add createPlayerBodySchema

--- src/modules/players/players.schemas.ts
+++ src/modules/players/players.schemas.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const createPlayerBodySchema = z.object({
   name: z.string().min(2).max(25),
-  known_as: z.string().min(2).max(25),
+  known_as: z.string().min(2).max(25).optional(),
   nationality: z.string().optional(),
   active: z.boolean().default(true),
 
