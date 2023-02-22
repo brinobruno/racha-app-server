ec6f1a4cd6b29bbb34c72e8387c29af6c42e804c
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-22 19:14:24 -0300

    test: Should be able to list a player

--- src/__tests__/players.spec.ts
+++ src/__tests__/players.spec.ts
@@ -183,4 +183,61 @@ describe('Players routes', () => {
       ]),
     )
   })
+
+  it('Should be able to list a player', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/users/create')
+      .send({
+        email: USER_REPOSITORY.USER_EMAIL,
+        password: USER_REPOSITORY.USER_PASSWORD,
+      })
+      .expect(201)
+
+    const cookies = createUserResponse.get('Set-Cookie')
+    const userId = createUserResponse.body.id
+
+    const createTeamResponse = await request(app.server)
+      .post(`/users/teams/create/${userId}`)
+      .set('Cookie', cookies)
+      .send({
+        title: TEAM_REPOSITORY.TEAM_TITLE,
+        owner: TEAM_REPOSITORY.TEAM_OWNER,
+        badge_url: TEAM_REPOSITORY.TEAM_BADGE_URL,
+        user_id: userId,
+      })
+      .expect(201)
+
+    const teamId = createTeamResponse.body.id
+
+    const createdPlayerResponse = await request(app.server)
+      .post(`/users/teams/players/create/${teamId}`)
+      .set('Cookie', cookies)
+      .send({
+        name: PLAYER_REPOSITORY.PLAYER_NAME,
+        known_as: PLAYER_REPOSITORY.PLAYER_KNOWN_AS,
+        nationality: PLAYER_REPOSITORY.PLAYER_NATIONALITY,
+        position: PLAYER_REPOSITORY.PLAYER_POSITION,
+        overall: PLAYER_REPOSITORY.PLAYER_OVERALL,
+        team_id: teamId,
+      })
+      .expect(201)
+
+    const playerId = createdPlayerResponse.body.createdPlayer.id
+
+    const getPlayerResponse = await request(app.server)
+      .get(`/users/teams/players/one/${playerId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getPlayerResponse.body.player).toEqual(
+      expect.objectContaining({
+        name: PLAYER_REPOSITORY.PLAYER_NAME,
+        known_as: PLAYER_REPOSITORY.PLAYER_KNOWN_AS,
+        nationality: PLAYER_REPOSITORY.PLAYER_NATIONALITY,
+        position: PLAYER_REPOSITORY.PLAYER_POSITION,
+        overall: PLAYER_REPOSITORY.PLAYER_OVERALL,
+        team_id: teamId,
+      }),
+    )
+  })
 })
