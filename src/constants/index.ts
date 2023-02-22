7b855aa2332df71e4d4cd95a5b474a3ae376eec6
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-22 19:48:18 -0300

    chore: Add session id max age days amount to constans to avoid magic numbers

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,6 @@
 export namespace Constants {
+  export const SESSION_ID_MAX_AGE_DAYS_AMOUNT = 7
+
   export const DEFENDER_OR_ATTACKER_DIVISOR = 5
   export const NOT_DEFENDER_OR_ATTACKER_DIVISOR = 5.5
 }
