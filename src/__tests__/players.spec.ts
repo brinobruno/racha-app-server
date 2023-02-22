455ec074aee9115ee5ebfb679127e3c271daf33c
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-22 19:34:22 -0300

    test: Should be able to update a player by id if cookie is present

--- src/__tests__/players.spec.ts
+++ src/__tests__/players.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import request from 'supertest'
 import { execSync } from 'node:child_process'
 import {
@@ -286,5 +287,72 @@ describe('Players routes', () => {
       .expect(200)
 
     expect(deletePlayerResponse.statusCode).toBe(200)
+  })
+
+  it('Should be able to update a player by id if cookie is present', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/users/create')
+      .send({
+        email: USER_REPOSITORY.USER_EMAIL,
+        password: USER_REPOSITORY.USER_PASSWORD,
+      })
+
+    const cookies = createUserResponse.get('Set-Cookie')
+    const userId = createUserResponse.body.id
+
+    const createTeamResponse = await request(app.server)
+      .post(`/users/teams/create/${userId}`)
+      .set('Cookie', cookies)
+      .send({
+        title: TEAM_REPOSITORY.TEAM_TITLE,
+        owner: TEAM_REPOSITORY.TEAM_OWNER,
+        badge_url: TEAM_REPOSITORY.TEAM_BADGE_URL,
+      })
+      .expect(201)
+
+    const teamId = createTeamResponse.body.id
+
+    const createdPlayerResponse = await request(app.server)
+      .post(`/users/teams/players/create/${teamId}`)
+      .set('Cookie', cookies)
+      .send({
+        name: PLAYER_REPOSITORY.PLAYER_NAME,
+        known_as: PLAYER_REPOSITORY.PLAYER_KNOWN_AS,
+        nationality: PLAYER_REPOSITORY.PLAYER_NATIONALITY,
+        position: PLAYER_REPOSITORY.PLAYER_POSITION,
+        overall: PLAYER_REPOSITORY.PLAYER_OVERALL,
+        team_id: teamId,
+      })
+      .expect(201)
+
+    const playerId = createdPlayerResponse.body.createdPlayer.id
+
+    await request(app.server)
+      .put(`/users/teams/players/${playerId}`)
+      .set('Cookie', cookies)
+      .send({
+        name: PLAYER_REPOSITORY.UPDATED_PLAYER_NAME,
+        known_as: PLAYER_REPOSITORY.UPDATED_PLAYER_KNOWN_AS,
+        nationality: PLAYER_REPOSITORY.UPDATED_PLAYER_NATIONALITY,
+        position: PLAYER_REPOSITORY.UPDATED_PLAYER_POSITION,
+        overall: PLAYER_REPOSITORY.UPDATED_PLAYER_OVERALL,
+        team_id: teamId,
+      })
+      .expect(200)
+
+    const getPlayerResponse = await request(app.server)
+      .get(`/users/teams/players/one/${playerId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const { name, known_as, nationality, position, overall, team_id } =
+      getPlayerResponse.body.player
+
+    expect(name).toBe(PLAYER_REPOSITORY.UPDATED_PLAYER_NAME)
+    expect(known_as).toBe(PLAYER_REPOSITORY.UPDATED_PLAYER_KNOWN_AS)
+    expect(nationality).toBe(PLAYER_REPOSITORY.UPDATED_PLAYER_NATIONALITY)
+    expect(position).toBe(PLAYER_REPOSITORY.UPDATED_PLAYER_POSITION)
+    expect(overall).toBe(PLAYER_REPOSITORY.UPDATED_PLAYER_OVERALL)
+    expect(team_id).toBe(teamId)
   })
 })
