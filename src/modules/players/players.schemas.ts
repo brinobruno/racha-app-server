d6aef7620ed354833e1e2e90e55329c333818993
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-22 17:44:27 -0300

    chore: Add updateTeamBodySchema

--- src/modules/players/players.schemas.ts
+++ src/modules/players/players.schemas.ts
@@ -28,6 +28,33 @@ export const createPlayerBodySchema = z.object({
   physical: z.number().int().optional(),
 })
 
+export const updatePlayerBodySchema = z.object({
+  name: z.string().min(2).max(25),
+  known_as: z.string().min(2).max(25).optional(),
+  nationality: z.string().optional(),
+  active: z.boolean().default(true),
+
+  position: z.enum([
+    'GK',
+    'CB',
+    'LB',
+    'RB',
+    'CDM',
+    'CM',
+    'CAM',
+    'LW',
+    'RW',
+    'ST',
+  ]),
+  overall: z.number().int().optional(),
+  pace: z.number().int().optional(),
+  shooting: z.number().int().optional(),
+  passing: z.number().int().optional(),
+  dribbling: z.number().int().optional(),
+  defending: z.number().int().optional(),
+  physical: z.number().int().optional(),
+})
+
 export enum DEFENDER_OR_ATTACKER_POSITIONS {
   CB = 'CB',
   LW = 'LW',
