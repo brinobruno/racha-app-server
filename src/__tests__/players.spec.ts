7a4bf31bc8c8dce97868fddc6b7d50117ebda953
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-22 15:40:43 -0300

    tests: Should be able to list all platers

--- src/__tests__/players.spec.ts
+++ src/__tests__/players.spec.ts
@@ -1,6 +1,13 @@
 import request from 'supertest'
 import { execSync } from 'node:child_process'
-import { describe, it, beforeAll, afterAll, beforeEach } from '@jest/globals'
+import {
+  describe,
+  it,
+  beforeAll,
+  afterAll,
+  beforeEach,
+  expect,
+} from '@jest/globals'
 
 import { app } from '../app'
 import {
@@ -69,5 +76,61 @@ describe('Players routes', () => {
         team_id: teamId,
       })
       .expect(201)
+  })
+
+  it('Should be able to list all players', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/users/create')
+      .send({
+        email: USER_EMAIL,
+        password: USER_PASSWORD,
+      })
+      .expect(201)
+
+    const cookies = createUserResponse.get('Set-Cookie')
+    const userId = createUserResponse.body.id
+
+    const createTeamResponse = await request(app.server)
+      .post(`/users/teams/create/${userId}`)
+      .set('Cookie', cookies)
+      .send({
+        title: TEAM_TITLE,
+        owner: TEAM_OWNER,
+        badge_url: TEAM_BADGE_URL,
+        user_id: userId,
+      })
+      .expect(201)
+
+    const teamId = createTeamResponse.body.id
+
+    await request(app.server)
+      .post(`/users/teams/players/create/${teamId}`)
+      .set('Cookie', cookies)
+      .send({
+        name: PLAYER_NAME,
+        known_as: PLAYER_KNOWN_AS,
+        nationality: PLAYER_NATIONALITY,
+        position: PLAYER_POSITION,
+        overall: PLAYER_OVERALL,
+        team_id: teamId,
+      })
+      .expect(201)
+
+    const getPlayersResponse = await request(app.server)
+      .get('/users/teams/players/all')
+      .expect(200)
+
+    expect(getPlayersResponse.body.players).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: PLAYER_NAME,
+          known_as: PLAYER_KNOWN_AS,
+          nationality: PLAYER_NATIONALITY,
+          position: PLAYER_POSITION,
+          overall: PLAYER_OVERALL,
+          team_id: teamId,
+        }),
+      ]),
+    )
   })
 })
