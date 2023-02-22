ce9fa8635706f34f0c27cb13f18cb19158b0bb96
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-22 18:46:02 -0300

    feat: Add team_repository set of exports

--- src/mockup-repository/index.ts
+++ src/mockup-repository/index.ts
@@ -4,26 +4,18 @@ const USER_PASSWORD = 'weakpassword123'
 const USER_UPDATED_EMAIL = 'updated@jest.com'
 const USER_UPDATED_PASSWORD = 'strongpassword456'
 
-/* Teams */
-const TEAM_TITLE = 'Blackburn Bosque'
-const TEAM_OWNER = '777 Partners'
-const TEAM_BADGE_URL = 'https://github.com/brinobruno.png'
-const TEAM_UPDATED_TITLE = 'Queens Bosque Rangers'
-const TEAM_UPDATED_OWNER = 'John Textor'
-const TEAM_UPDATED_BADGE_URL =
-  'https://github.githubassets.com/images/modules/profile/achievements/quickdraw-default.png'
+export { USER_EMAIL, USER_PASSWORD, USER_UPDATED_EMAIL, USER_UPDATED_PASSWORD }
 
-export {
-  USER_EMAIL,
-  USER_PASSWORD,
-  USER_UPDATED_EMAIL,
-  USER_UPDATED_PASSWORD,
-  TEAM_TITLE,
-  TEAM_OWNER,
-  TEAM_BADGE_URL,
-  TEAM_UPDATED_TITLE,
-  TEAM_UPDATED_OWNER,
-  TEAM_UPDATED_BADGE_URL,
+export const TEAM_REPOSITORY = {
+  /* Team */
+  TEAM_TITLE: 'Blackburn Bosque',
+  TEAM_OWNER: '777 Partners',
+  TEAM_BADGE_URL: 'https://github.com/brinobruno.png',
+
+  /* Updated team */
+  UPDATED_TEAM_TITLE: 'Queens Bosque Rangers',
+  UPDATED_TEAM_OWNER: 'John Textor',
+  UPDATED_TEAM_BADGE_URL: 'https://github.com/brinobruno.jpg',
 }
 
 export const PLAYER_REPOSITORY = {
